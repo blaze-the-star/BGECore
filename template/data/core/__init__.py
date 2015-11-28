7c969eda234e921f244598d535665c563d34c870
Robert Planas <elmeunick9@gmail.com>
2015-11-28 17:06:44 +0100

    Clean

--- template/data/core/__init__.py
+++
@@ -1,5 +0,0 @@
-import core.interface
-import core.utils
-import core.game
-import core.dynamic
-import core.sequencer
--- template/data/core/interface/module.py
+++
@@ -1,6 +0,0 @@
-from core import utils
-from script import constant
-
-window = None
-widget_dict = {}
-scene = utils.getSceneByName(constant.CORE_SCENE_GUI)
